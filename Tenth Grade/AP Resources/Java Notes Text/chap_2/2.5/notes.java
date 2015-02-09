4a0edeec556044fb6ce464ff2542b16bd861f1b0
Jesse Evers <jesse27999@gmail.com>
2015-02-09 11:10:04 -0500

    AP exercises

---
+++ Tenth Grade/AP Resources/Java Notes Text/chap_2/2.5/notes.java
@@ -0,0 +1,5 @@
+/\/\/\Section 2.5 Details of Expressions/\/\/\
+==============================================
+
+/* An expression is a piece of program code that represents or computes a
+ * value. It can be a literal, a variable, a function, etc. */
